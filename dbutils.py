49baf63d10b5ea5da2a0b597696bcac289fa3c3c
zhangbingxiang <zbx13@163.com>
2019-12-06 15:37:02 +0800

    添加mysql支持

--- dbutils.py
+++ dbutils.py
@@ -3,19 +3,24 @@
 
 import MySQLdb
 
-# 打开数据库连接
-db = MySQLdb.connect("11.11.1.80", "root", "admin", "zycloud_admin", charset='utf8')
 
-# 使用cursor()方法获取操作游标
-cursor = db.cursor()
+class DB:
+    def __init__(self):
+        # 打开数据库连接
+        self.db = MySQLdb.connect("127.0.0.1", "root", "admin", "sgba", charset='utf8')
+        # 使用cursor()方法获取操作游标
+        self.cursor = self.db.cursor()
 
-# 使用execute方法执行SQL语句
-cursor.execute("SELECT VERSION()")
+    def execute(self, sql):
+        # 使用execute方法执行SQL语句
+        self.cursor.execute(sql)
 
-# 使用 fetchone() 方法获取一条数据
-data = cursor.fetchone()
+    def fetchone(self):
+        # 使用 fetchone() 方法获取一条数据
+        return self.cursor.fetchone()
 
-print("Database version : %s " % data)
+    def close(self):
+        self.db.close()
 
-# 关闭数据库连接
-db.close()
+    def commit(self):
+        self.db.commit()
