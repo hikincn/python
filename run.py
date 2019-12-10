9223dcdba7c9c13199d26958b2e80ac84c71499d
zhangbingxiang <zbx13@163.com>
2019-12-10 14:58:04 +0800

    添加定时任务

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+
+from apscheduler.schedulers.blocking import BlockingScheduler
 from KPI import KPI
 from deal import Deal
 from GDZCTZ import GDZCTZ
@@ -11,10 +13,14 @@ from shibor import Shibor
 from stock import Stock
 from waihui import Waihui
 
+
 if __name__ == '__main__':
 
-    spiders = [
-        KPI()
-    ]
-    for spider in spiders:
-        spider.__getattribute__("run")()
+    scheduler = BlockingScheduler()
+
+    # 添加任务作业，本任务设置为 每10秒钟执行一次
+    scheduler.add_job(Shibor().run, 'cron', hour='*', minute='*', second='*/10')
+    # 添加任务作业，本任务设置为 每天12点执行一次
+    scheduler.add_job(Deal().run, 'cron', hour='12', minute='0', second='0')
+
+    scheduler.start()
