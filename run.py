683d2fbcf6162dbc8f36070921c5a3f31ef29065
zhongjixin <zhongjixin@163.com>
2019-12-12 21:28:29 +0800

    ok

--- run.py
+++ run.py
@@ -3,24 +3,27 @@
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from KPI import KPI
-from deal import Deal
-from GDZCTZ import GDZCTZ
+from qihuo_dl import qihuo_dl
 from MoneySupply import MoneySupply
-from PMI import PMI
-from PPI import PPI
-from delaymarket import Delaymarket
+from qihuo_sh import qihuo_sh
 from shibor import Shibor
 from stock import Stock
-from waihui import Waihui
+from hl import Hl
 
 
 if __name__ == '__main__':
 
-    scheduler = BlockingScheduler()
+    #scheduler = BlockingScheduler()
 
     # 添加任务作业，本任务设置为 每10秒钟执行一次
-    scheduler.add_job(Shibor().run, 'cron', hour='*', minute='*', second='*/10')
+    #scheduler.add_job(Shibor().run, 'cron', hour='*', minute='*', second='*/10')
     # 添加任务作业，本任务设置为 每天12点执行一次
-    scheduler.add_job(Deal().run, 'cron', hour='12', minute='0', second='0')
+    #scheduler.add_job(Deal().run, 'cron', hour='12', minute='0', second='0')
 
-    scheduler.start()
+    #scheduler.start()
+
+    spiders = [
+        Shibor()
+    ]
+    for spider in spiders:
+        spider.__getattribute__("run")()
